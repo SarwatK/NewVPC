f32ac3d334a25da8ecda8fbe9714547b390930b2
SarwatK <sarwat.kadri@capgemini.com>
2024-08-28 12:29:01 +0530

    vpccreated2

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 resource "aws_vpc" "my_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
